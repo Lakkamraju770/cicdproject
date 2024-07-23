91fa66ad773e890f103b3302d780aad414c40d13
Lakkamraju Raju <kshatriyabalu123@gmail.com>
2024-07-23 14:06:10 +0530

    Terraform success

---
+++ terraform_code/Ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1" 
+    
+}
+
+resource "aws_instance" "demo-server"{
+    ami = "ami-0427090fd1714168b"
+    instance_type ="t2.micro"
+    key_name = "Valaxyproject"
+}
